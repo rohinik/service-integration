30ea2290f04a1e69d7778339374e25f028cb8809
Rohini Kulkarni <rohini.kulkarni@gfs.com>
2019-07-24 14:01:52 -0500

    Initial commit with sample test:

---
+++ src/test/java/com/api/integtation/serviceintegration/SampleAPITest.java
@@ -0,0 +1,20 @@
+package com.api.integtation.serviceintegration;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+
+public class SampleAPITest {
+
+  RestTemplate restTemplate = new RestTemplate();
+
+  @Test
+  public void shouldVerifySampleEndpoint(){
+    String result = restTemplate
+        .getForObject("https://humming-hypnotisation.cfapps.io", String.class);
+
+    assertThat(result).isEqualTo("Hello from the review environment");
+  }
+
+}
